313ad4be88cf119afc0e1c831d23abbf88d422c5
dylanprem <[dylanprem90@gmail.com]>
2018-03-09 17:04:27 -0500

    styling

--- src/components/jobs/viewJob.js
+++ src/components/jobs/viewJob.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import JobFeed from '../feeds/JobFeed';
 import PostJobSeeker from '../jobseekers/PostJobSeeker';
 import { Modal } from 'react-bootstrap';
+import './style.css'
 
 
 
@@ -52,7 +53,7 @@ class ViewJob extends Component{
 	  }
 
 	  handleSubmit(e) {
-	  e.preventDefault();
+	  
 	  const JobPostsCandidatesRef = firebase.database().ref('JobPosts' + '/' + this.state.currentJob + '/' + 'postsFromUsers');
 	  
 	  
@@ -60,7 +61,7 @@ class ViewJob extends Component{
 	  	uid: this.state.authUser.uid,
 	  	firstName: this.state.firstName,
 	  	lastName: this.state.lastName,
-	  	email: this.state.email,
+	  	email: this.state.authUser.email,
 	    position: this.state.position,
 	    state: this.state.state,
 	    city: this.state.city,
@@ -113,7 +114,7 @@ class ViewJob extends Component{
 	JobPostsCandidatesRef.once('value', (snapshot) => {
 		let postsFromUsers = snapshot.val();
 		let newState = [];
-for (let post in postsFromUsers) {
+		for (let post in postsFromUsers) {
 	      newState.push({
 	        id: post,
 	        firstName: postsFromUsers[post].firstName,
@@ -145,197 +146,177 @@ for (let post in postsFromUsers) {
 	render(){
 		return(
 			<div>
-			<div className='col-md-6'>
-				{this.state.JobPosts.map((post) => {
-			    		return(
-	                    <div className="success text-center" key={post.id}>
-					       <h3>Company: </h3>
-					       <p>{post.companyName}</p>
-					       
-					       <h3>Email:</h3>
-					       <p>{post.email}</p>
+				<div className='col-md-12 bg-primary company-info'>
+					{this.state.JobPosts.map((post) => {
+				    		return(
+		                    <div className="success text-center" key={post.id}>
+						       <h3>Company: </h3>
+						       <p>{post.companyName}</p>
+						       
+						       <h3>Email:</h3>
+						       <p>{post.email}</p>
 
-					       <h3>Phone:</h3>
-					       <p>{post.phone}</p>
+						       <h3>Phone:</h3>
+						       <p>{post.phone}</p>
 
-					       <h3>Position</h3>
-					       <p>{post.position}</p>
+						       <h3>Position</h3>
+						       <p>{post.position}</p>
 
-					       <h3>Job Location</h3>
-					       <p>{post.address}</p>
+						       <h3>Job Location</h3>
+						       <p>{post.address}</p>
 
-					       <h3>About the Job:</h3>
-					       <p>{post.about}</p>
-					       <Link target='_new' className='btn btn-primary' to={`${post.applyLink}`}>Apply</Link>
-				        </div>
+						       <h3>About the Job:</h3>
+						       <p>{post.about}</p>
+						       <Link target='_new' className='btn btn-info' to={`${post.applyLink}`}>Apply</Link>
+					        </div>
 
-				         );
-	        		})}
-	        	       
-			</div>
-			<div className='col-md-4'>
-				<h3 className='text-center'>Interested in this job? Tell us about yourself.</h3>
-				<form onSubmit={this.handleSubmit}>
-					<div className='form-group'>
-						<label>First Name:</label>
-						<input name='firstName' type='text' onChange={this.handleChange} value={this.state.firstName} className='form-control' />
-					</div>
-					<div className='form-group'>
-						<label>Last Name:</label>
-						<input name='lastName' type='text' onChange={this.handleChange} value={this.state.lastName} className='form-control' />
-					</div>
-					<div className='form-group'>
-						<label>I am a:</label>
-						<select required className='form-control' name='position' onChange={this.handleChange} value={this.state.position}>
-							<option value="" disabled selected>Select an option</option>
-							<option value="Front End Developer">Front End Developer</option>
-							<option value="Back End Developer">Back End Developer</option>
-							<option value="Full Stack Developer">Full Stack Developer</option>
-						</select>
-					</div>
-					<div className='form-group'>
-						<label>I'm located in:</label>
-						<p>State</p>
-						<select required className='form-control' name='state' onChange={this.handleChange} value={this.state.state}>
-							<option value="" disabled selected>Select an option</option>
-							<option value="AL">Alabama</option>
-							<option value="AK">Alaska</option>
-							<option value="AZ">Arizona</option>
-							<option value="AR">Arkansas</option>
-							<option value="CA">California</option>
-							<option value="CO">Colorado</option>
-							<option value="CT">Connecticut</option>
-							<option value="DE">Delaware</option>
-							<option value="DC">District Of Columbia</option>
-							<option value="FL">Florida</option>
-							<option value="GA">Georgia</option>
-							<option value="HI">Hawaii</option>
-							<option value="ID">Idaho</option>
-							<option value="IL">Illinois</option>
-							<option value="IN">Indiana</option>
-							<option value="IA">Iowa</option>
-							<option value="KS">Kansas</option>
-							<option value="KY">Kentucky</option>
-							<option value="LA">Louisiana</option>
-							<option value="ME">Maine</option>
-							<option value="MD">Maryland</option>
-							<option value="MA">Massachusetts</option>
-							<option value="MI">Michigan</option>
-							<option value="MN">Minnesota</option>
-							<option value="MS">Mississippi</option>
-							<option value="MO">Missouri</option>
-							<option value="MT">Montana</option>
-							<option value="NE">Nebraska</option>
-							<option value="NV">Nevada</option>
-							<option value="NH">New Hampshire</option>
-							<option value="NJ">New Jersey</option>
-							<option value="NM">New Mexico</option>
-							<option value="NY">New York</option>
-							<option value="NC">North Carolina</option>
-							<option value="ND">North Dakota</option>
-							<option value="OH">Ohio</option>
-							<option value="OK">Oklahoma</option>
-							<option value="OR">Oregon</option>
-							<option value="PA">Pennsylvania</option>
-							<option value="RI">Rhode Island</option>
-							<option value="SC">South Carolina</option>
-							<option value="SD">South Dakota</option>
-							<option value="TN">Tennessee</option>
-							<option value="TX">Texas</option>
-							<option value="UT">Utah</option>
-							<option value="VT">Vermont</option>
-							<option value="VA">Virginia</option>
-							<option value="WA">Washington</option>
-							<option value="WV">West Virginia</option>
-							<option value="WI">Wisconsin</option>
-							<option value="WY">Wyoming</option>
-						</select>	
-					</div>
-					<div className='form-group'>
-						<p>City</p>
-						<input required className='form-control' name='city' onChange={this.handleChange} value={this.state.city}/>
-					</div>
-					<div className='form-group'>
-						<label>Are you willing to relocate?</label>
-						<select required className='form-control' name='relocate' onChange={this.handleChange} value={this.state.relocate}>
-							<option value="" disabled selected>Select an option</option>
-							<option value="Willing to relocate">Willing to relocate.</option>
-							<option value="Willing to relocate">Does not want to relocate.</option>
-						</select>
-					</div>
+					         );
+		        		})}
+		        	       
+				</div>
+				<div className='col-md-12 post-box'>
+					<h3 className='text-center'>Interested in this job? Tell us about yourself.</h3>
+					<form className='col-md-4 col-md-offset-4' onSubmit={this.handleSubmit}>
+						<div className='form-group'>
+							<label>First Name:</label>
+							<input name='firstName' type='text' onChange={this.handleChange} value={this.state.firstName} className='form-control' />
+						</div>
+						<div className='form-group'>
+							<label>Last Name:</label>
+							<input name='lastName' type='text' onChange={this.handleChange} value={this.state.lastName} className='form-control' />
+						</div>
+						<div className='form-group'>
+							<label>I am a:</label>
+							<select required className='form-control' name='position' onChange={this.handleChange} value={this.state.position}>
+								<option value="" disabled selected>Select an option</option>
+								<option value="Front End Developer">Front End Developer</option>
+								<option value="Back End Developer">Back End Developer</option>
+								<option value="Full Stack Developer">Full Stack Developer</option>
+							</select>
+						</div>
+						<div className='form-group'>
+							<label>I'm located in:</label>
+							<p>State</p>
+							<select required className='form-control' name='state' onChange={this.handleChange} value={this.state.state}>
+								<option value="" disabled selected>Select an option</option>
+								<option value="AL">Alabama</option>
+								<option value="AK">Alaska</option>
+								<option value="AZ">Arizona</option>
+								<option value="AR">Arkansas</option>
+								<option value="CA">California</option>
+								<option value="CO">Colorado</option>
+								<option value="CT">Connecticut</option>
+								<option value="DE">Delaware</option>
+								<option value="DC">District Of Columbia</option>
+								<option value="FL">Florida</option>
+								<option value="GA">Georgia</option>
+								<option value="HI">Hawaii</option>
+								<option value="ID">Idaho</option>
+								<option value="IL">Illinois</option>
+								<option value="IN">Indiana</option>
+								<option value="IA">Iowa</option>
+								<option value="KS">Kansas</option>
+								<option value="KY">Kentucky</option>
+								<option value="LA">Louisiana</option>
+								<option value="ME">Maine</option>
+								<option value="MD">Maryland</option>
+								<option value="MA">Massachusetts</option>
+								<option value="MI">Michigan</option>
+								<option value="MN">Minnesota</option>
+								<option value="MS">Mississippi</option>
+								<option value="MO">Missouri</option>
+								<option value="MT">Montana</option>
+								<option value="NE">Nebraska</option>
+								<option value="NV">Nevada</option>
+								<option value="NH">New Hampshire</option>
+								<option value="NJ">New Jersey</option>
+								<option value="NM">New Mexico</option>
+								<option value="NY">New York</option>
+								<option value="NC">North Carolina</option>
+								<option value="ND">North Dakota</option>
+								<option value="OH">Ohio</option>
+								<option value="OK">Oklahoma</option>
+								<option value="OR">Oregon</option>
+								<option value="PA">Pennsylvania</option>
+								<option value="RI">Rhode Island</option>
+								<option value="SC">South Carolina</option>
+								<option value="SD">South Dakota</option>
+								<option value="TN">Tennessee</option>
+								<option value="TX">Texas</option>
+								<option value="UT">Utah</option>
+								<option value="VT">Vermont</option>
+								<option value="VA">Virginia</option>
+								<option value="WA">Washington</option>
+								<option value="WV">West Virginia</option>
+								<option value="WI">Wisconsin</option>
+								<option value="WY">Wyoming</option>
+							</select>	
+						</div>
+						<div className='form-group'>
+							<p>City</p>
+							<input required className='form-control' name='city' onChange={this.handleChange} value={this.state.city}/>
+						</div>
+						<div className='form-group'>
+							<label>Are you willing to relocate?</label>
+							<select required className='form-control' name='relocate' onChange={this.handleChange} value={this.state.relocate}>
+								<option value="" disabled selected>Select an option</option>
+								<option value="Willing to relocate">Willing to relocate.</option>
+								<option value="Willing to relocate">Does not want to relocate.</option>
+							</select>
+						</div>
 
-					<div className='form-group'>
-						<label>Tell us your strengths and weaknesses. Be sure to include specific technologies and frameworks.</label>
-						<textarea onChange={this.handleChange} value={this.state.about} className='form-control' rows="5" name='about' placeholder='e.g. I love Javascript, but PHP? ...not so great. I am great with NOSQL datatbases like Firebase and MongoDB, but I am not a fan of MySQL.'></textarea>
-					</div>
+						<div className='form-group'>
+							<label>Tell us your strengths and weaknesses. Be sure to include specific technologies and frameworks.</label>
+							<textarea onChange={this.handleChange} value={this.state.about} className='form-control' rows="5" name='about' placeholder='e.g. I love Javascript, but PHP? ...not so great. I am great with NOSQL datatbases like Firebase and MongoDB, but I am not a fan of MySQL.'></textarea>
+						</div>
 
-					<div className='form-group'>
-					<div class="input-group">
-					    <span class="input-group-addon">GitHub url</span>
-					    <input id="msg" type="text" class="form-control" name="github" onChange={this.handleChange} value={this.state.github} placeholder="e.g. https://github.com/mygithub" />
-					</div>
+						<div className='form-group'>
+						<div class="input-group">
+						    <span class="input-group-addon">GitHub url</span>
+						    <input id="msg" type="text" class="form-control" name="github" onChange={this.handleChange} value={this.state.github} placeholder="e.g. https://github.com/mygithub" />
+						</div>
 
-					<div class="input-group">
-					    <span class="input-group-addon">LinkedIn url</span>
-					    <input id="msg" type="text" class="form-control" name="linkedin" onChange={this.handleChange} value={this.state.linkedin} placeholder="e.g. https://linkedin.com/in/mylinkedin" />
-					</div>
-					</div>
-					<input type='submit' className='btn btn-primary btn-block' value='Post' />
-				</form>
+						<div class="input-group">
+						    <span class="input-group-addon">LinkedIn url</span>
+						    <input id="msg" type="text" class="form-control" name="linkedin" onChange={this.handleChange} value={this.state.linkedin} placeholder="e.g. https://linkedin.com/in/mylinkedin" />
+						</div>
+						</div>
+						<input type='submit' className='btn btn-primary btn-block' value='Post' />
+					</form>
 
-			</div> 
-			<div className='col-md-12'>
-			<h3 className='text-center text-primary'>Members interested in this job:</h3>
-			<table className="table">
-				    <thead>
-				      <tr>
-				      	<th>Name</th>
-				        <th>Email</th>
-				        <th>Position</th>
-				        <th>Location</th>
-				        <th>Pitch</th>
-				        <th>GitHub</th>
-				        <th>LinkedIn</th>
-				      </tr>
-				    </thead>
-				    <tbody>
-				    {this.state.postsFromUsers.map((post) => {
-    		  			return(
-					    <tr className='info' key={post.id}>
-	                      <td className='text-warning'>{post.firstName} {post.lastName}</td>
-	                      <td>{post.email}</td>
-	                      <td>{post.position}</td>
-	                      <td>{post.city}, {post.state}</td>
-	                      <td><button className='btn-info btn btn-sm' onClick={this.handleShow}>View</button></td>
-	                      <td><Link target='_new' className='btn btn-primary btn-sm' to={`${post.github}`}><small>GitHub</small></Link></td>
-					      <td><Link target='_new' className='btn btn-primary btn-sm' to={`${post.linkedin}`}><small>LinkedIn</small></Link></td>  
-	                    </tr>
-	                    
-		                    );
-	          			})}         
-				    </tbody>
-				</table>
-				<div>
-				{this.state.postsFromUsers.map((post) => {
-    		  		return(
-				<Modal show={this.state.show} onHide={this.handleClose}>
-				
-		          <Modal.Header closeButton>
-		            <Modal.Title>Why this job is for me</Modal.Title>
-		          </Modal.Header>
-		         
-		          <Modal.Body>
-		          <p>{post.about}</p>
-		          </Modal.Body>
-
-		          <Modal.Footer>
-		            <button className='btn btn-danger' onClick={this.handleClose}>Close</button>
-		          </Modal.Footer>
-		            
-		        </Modal>
-		          );
-	          		})}
-		        </div>
+				</div> 
+				<div className='col-md-12 members-posted'>
+					<h1 className='text-center text-primary'>Members interested in this job:</h1>
+					<table className="table">
+						    <thead>
+						      <tr>
+						      	<th>Name</th>
+						        <th>Email</th>
+						        <th>Position</th>
+						        <th>Location</th>
+						        <th>Details</th>
+						        <th>GitHub</th>
+						        <th>LinkedIn</th>
+						      </tr>
+						    </thead>
+						    <tbody>
+						    {this.state.postsFromUsers.map((post) => {
+		    		  			return(
+							    <tr className='info' key={post.id}>
+			                      <td className='text-warning'>{post.firstName} {post.lastName}</td>
+			                      <td><strong className='text-primary'>{post.email}</strong></td>
+			                      <td>{post.position}</td>
+			                      <td>{post.city}, {post.state}</td>
+			                      <td><button className='btn btn-info btn-sm'>View Details</button></td>
+			                      <td><Link target='_new' className='btn btn-primary btn-sm' to={`${post.github}`}><small>GitHub</small></Link></td>
+							      <td><Link target='_new' className='btn btn-primary btn-sm' to={`${post.linkedin}`}><small>LinkedIn</small></Link></td>  
+			                    </tr>
+			                    
+				                    );
+			          			})}         
+						    </tbody>
+		            </table>		  	
+		    		  	
 				</div>
 			</div>
 		);
